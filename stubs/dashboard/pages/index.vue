4fe66a2b6ce9ac3fc25636c6493bd5d730895924
Romain Fouilland <romain.fd@gmail.com>
2022-12-16 00:17:45 +0100

    ⚙ Initial Nuxt app for dashboard

---
+++ stubs/dashboard/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">Dashboard</v-card-title>
+        <v-card-text>
+          <v-alert type="info">
+            This is the dashboard page. You can find it in <code>resources/js/stubs/dashboard/pages/index.vue</code>
+          </v-alert>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
